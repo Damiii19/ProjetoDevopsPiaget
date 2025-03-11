bf30df28e0dc9eab377c77705f1103723b237352
Damiii19 <damares.silva.300@gmail.com>
2025-03-10 21:23:21 -0300

    feat: Logica geral

---
+++ ApiPiaget/models/Aluno.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiPiaget.models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? RGM { get; set; }
+        public string? CPF { get; set; }
+    }
+}
